05e030a409b9cf2259cfc46854aeb3357f95ba0f
LLons <50717859+LionsDevelopment@users.noreply.github.com>
2024-09-21 15:33:19 -0500

    Added posts that get the teams information, also made it to where the teams can't be saved unless both of the fields are filled out.

--- app/app.py
+++ app/app.py
@@ -1,21 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/editMode")
-def addPlayer():
-    return render_template("add-player/add-player.html")
-
+RED_TEAM_PLAYERS = []
+GREEN_TEAM_PLAYERS = []
 
 @app.route("/")
 def index():
     return render_template("splash-screen/splash-screen.html")
 
-
 @app.route("/gameAction")
 def gameAction():
     return render_template("game-action/game-action.html")
+
+@app.route("/editMode")
+def addPlayer():
+    return render_template("add-player/add-player.html")
+
+@app.route("/submit-red", methods = ["POST"])
+def submitRedTeams() -> None:
+     for i in range(1, 21):
+        player_name = request.form.get(f"player_name_{i}")
+        player_id = request.form.get(f"player_id_{i}")
+        if player_name and player_id:
+            RED_TEAM_PLAYERS.append({f"name_{i}": player_name, f"id_{i}": player_id})
+     return "", 204
+
+
+@app.route("/submit-green", methods = ["POST"])
+def submitGreenTeams() -> None:
+     for i in range(1, 21):
+        player_name = request.form.get(f"player_name_{i}")
+        player_id = request.form.get(f"player_id_{i}")
+        if player_name and player_id:
+            GREEN_TEAM_PLAYERS.append({f"name_{i}": player_name, f"id_{i}": player_id})
+     return "", 204
 
 
 if __name__ == "__main__":
--- app/static/script.js
+++ app/static/script.js
@@ -32,6 +32,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 3000);
 });
 
+function checkEditScreenInputs(event) { 
+    const form = event.target;
+    let player_name, player_id;
+
+    for(let i = 1; i<= 20; i++){
+        player_name = form.querySelector(`#player_name_${i}`).value.trim();
+        player_id = form.querySelector(`#player_id_${i}`).value.trim();
+
+        if((player_name === "" && player_id === "") || (player_name !== "" && player_id !== "")){
+            return;
+        }
+         else if (player_name == "" || player_id === "") {
+            alert('Please fill out all fields');
+            event.preventDefault();
+            return;
+        }
+    }
+};
+
+
 // document.addEventListener('DOMContentLoaded', () => {
 //     document.querySelector('button').addEventListener('click', () => {
 //         window.location.href = '/editMode';
