2801d0357776ab5fb57852deb476c727f5c660a2
Jackson H <0x01fe@0x01fe.net>
2024-09-22 00:38:59 -0500

    return none instead of throwing index out of bounds error

--- app/database.py
+++ app/database.py
@@ -12,7 +12,11 @@ def get_player_by_id(player_id: int):
 
         cur.execute("SELECT * FROM players WHERE id = %s", (player_id,))
 
-        return cur.fetchall()[0]
+        data = cur.fetchall()
+        if data:
+            return data[0]
+        
+        return None
 
 # Returns an empty list if nothing is found
 def get_player_by_codename(codename: str):
@@ -21,7 +25,11 @@ def get_player_by_codename(codename: str):
 
         cur.execute("SELECT * FROM players WHERE codename = %s", (codename,))
 
-        return cur.fetchall()[0]
+        data = cur.fetchall()
+        if data:
+            return data[0]
+        
+        return None
 
 def add_codename(player_id: int, codename: str):
     with psycopg.connect(CONNECTION_STRING) as conn:
