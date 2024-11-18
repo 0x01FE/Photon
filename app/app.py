679a8daafecef2cb81ffc4c7616b5388d6cd77e0
LLons <50717859+LionsDevelopment@users.noreply.github.com>
2024-11-17 22:06:44 -0600

    Fixing up the game over screen

--- app/app.py
+++ app/app.py
@@ -35,14 +35,6 @@ def index():
 
 @app.route("/gameAction")
 def gameAction():
-    # actions = [
-    #     "Scooby Doo hit Opus",
-    #     "Scooby Doo hit Opus",
-    #     "Scooby Doo hit Opus",
-    #     "Opus hit Scooby Doo",
-    #     "Opus hit the Base",
-    #     "Opus hit Scooby Doo",
-    # ]
     redPlayers = [player.codename for _, player in s.red_players.items()]
     greenPlayers = [player.codename for _, player in s.green_players.items()]
 
@@ -58,9 +50,11 @@ def addPlayer():
 
 @app.route("/gameOver")
 def gameOver():
-    # redPlayers= ['player1', 'player2', 'player3', 'player4', 'player5', 'player6', 'player7', 'player8', 'player9', 'player10']
-    # greenPlayers= ['player1', 'player2', 'player3', 'player4', 'player5', 'player6', 'player7', 'player8', 'player9', 'player10']
-    return render_template("game-over.html", redPlayers=red_players, greenPlayers=green_players)
+    redPlayers = [player for _, player in s.red_players.items()]
+    greenPlayers = [player for _, player in s.green_players.items()]
+    totalRedScore = s.redScore
+    totalGreenScore = s.greenScore
+    return render_template("game-over.html", redPlayers=redPlayers, greenPlayers=greenPlayers, totalRedScore=totalRedScore, totalGreenScore=totalGreenScore)
 
 @app.route("/clearTeams", methods=["POST"])
 def clearAllTeams():
